77684c4646f255ebe93a428b59031923bba67b2b
Kayvan Vakili <kvakili813@gmail.com>
2018-03-30 23:58:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
   grill = []
 holiday_hash.each do |season, holiday|
   holiday.each do |day, supplies|
-  [] << supplies.include("BBQ")
+  [] << supplies.include?("BBQ")
 end
   end
   grill
