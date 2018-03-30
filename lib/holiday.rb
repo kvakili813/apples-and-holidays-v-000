672e3eab00231deb3aab7627e782686ebb1f188f
Kayvan Vakili <kvakili813@gmail.com>
2018-03-30 23:41:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,5 +82,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash["BBQ"]
+holiday_hash.each do |season, holiday|
+  holiday.each do |day, supplies|
+    if supplies == "BBQ"
+end
+  end
+    end
 end
